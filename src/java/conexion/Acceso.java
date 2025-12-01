ebfa27084c2b172862316273ea091c4c2b2ea240
zoedays <zoedaych@gmail.com>
2025-12-01 17:07:33 -0500

    Change database connection password
    Updated database connection password for security.

--- src/java/conexion/Acceso.java
+++ src/java/conexion/Acceso.java
@@ -11,7 +11,7 @@ public class Acceso {
         try {
             String url = "jdbc:mysql://bspupdjree1hm2yg9arj-mysql.services.clever-cloud.com:3306/bspupdjree1hm2yg9arj?useSSL=true&requireSSL=true";
             String user = "ufz4rrgmwskemty2";
-            String password = "ufz4rrgmwskemty2";
+            String password = "ohsg5IiySrtz8ONrJiHU";
 
             Class.forName("com.mysql.cj.jdbc.Driver");
             cn = DriverManager.getConnection(url, user, password);
@@ -105,6 +105,7 @@ public class Acceso {
     }
 
 }
+
 
 
 
